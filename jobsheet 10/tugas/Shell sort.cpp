38d96319416e9410502158793633ac8815276783
ketherinfr27 <ketherinfr27@gmail.com>
2024-06-22 09:44:00 +0700

    jobsheet 10

---
+++ jobsheet 10/tugas/Shell sort.cpp
@@ -0,0 +1,40 @@
+//Created by 23343072_ketherin fathur rahma
+ 
+#include <stdio.h>
+
+void shellSort(int arr[], int n) 
+{
+  int gap = n / 2;
+  while (gap > 0) 
+  {
+    for (int i = gap; i < n; i++) 
+	{
+      int temp = arr[i];
+      int j = i;
+      while (j >= gap && arr[j - gap] > temp) 
+	  {
+        arr[j] = arr[j - gap];
+        j -= gap;
+      }
+      arr[j] = temp;
+    }
+    gap /= 2;
+  }
+}
+int main() 
+{
+  int arr[] = {15, 21, 4, 17, 30};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  printf("Array sebelum diurutkan: ");
+  for (int i = 0; i < n; i++) 
+  {
+    printf("%d ", arr[i]);
+  }
+  shellSort(arr, n);
+  printf("\nArray setelah diurutkan: ");
+  for (int i = 0; i < n; i++) 
+  {
+    printf("%d ", arr[i]);
+  }
+  return 0;
+}
